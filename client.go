f5a13d4180976adca5bb1d38475992a2743bb6ee
Francisco Castanho <castanho.jfc@gmail.com>
2023-04-26 23:49:52 +0100

    feat: create client based on api url

---
+++ client.go
@@ -0,0 +1,20 @@
+package client
+
+import (
+	"net/http"
+	"os"
+)
+
+type Client struct {
+	apiURL     string
+	httpClient *http.Client
+}
+
+func NewClient() *Client {
+	client := &Client{
+		apiURL:     os.Getenv("FORM3_ACCOUNT_API_URL"),
+		httpClient: http.DefaultClient,
+	}
+
+	return client
+}
---
+++ client_test.go
@@ -0,0 +1,15 @@
+package client
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestClient(t *testing.T) {
+	t.Run("can create client with API url", func(*testing.T) {
+		client := NewClient()
+
+		assert.NotNil(t, client)
+	})
+}
